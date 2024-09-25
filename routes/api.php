9baf91990db16e899ea8ac2204c9ae7a7e4df0b0
Jose Anicama <jose.anicama@dbtarget.pe>
2024-09-25 15:57:30 -0500

    chore: Update version

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UrlController;
 
 Route::resource('urls', UrlController::class);
+
+Route::get('version', function () {
+    return response()->json(['version' => '1.0.0']);
+});
