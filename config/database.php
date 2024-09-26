f1447d4185b1bc71b9eba2cb13116f85860be1ec
Jose Anicama <jose.anicama@dbtarget.pe>
2024-09-25 19:45:18 -0500

    refactor: define consts in database and logging configs

--- config/database.php
+++ config/database.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Str;
 
-const LOCALHOST = "127.0.0.1";
+if (!defined('LOCALHOST')) {
+    define('LOCALHOST', '127.0.0.1');
+}
 
 return [
 
--- config/logging.php
+++ config/logging.php
@@ -5,7 +5,9 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
 use Monolog\Processor\PsrLogMessageProcessor;
 
-const LARAVEL_LOGS = "logs/laravel.log";
+if (!defined('LARAVEL_LOGS')) {
+    define('LARAVEL_LOGS', 'logs/laravel.log');
+}
 
 return [
 
